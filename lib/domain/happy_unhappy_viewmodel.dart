4a47ceca2525b8edfad155d5dda84f4dee09b159
nekomochi123 <akasama5968+work@gmail.com>
2024-04-06 12:33:54 +0900

    [feature]幸せチェック

--- lib/domain/happy_unhappy_viewmodel.dart
+++ lib/domain/happy_unhappy_viewmodel.dart
@@ -3,6 +3,14 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 class RadioNotifier extends StateNotifier<int> {
   RadioNotifier() : super(0);
 
+  String get title {
+    if (state == 1) {
+      return '幸せ';
+    } else {
+      return '不幸';
+    }
+  }
+
   void update_happy(int value) {
     state = value;
   }
