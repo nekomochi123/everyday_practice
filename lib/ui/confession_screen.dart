082ebc16d8620c3d90fbd748fe47ca9ed16fc6e8
nekomochi123 <akasama5968+work@gmail.com>
2024-04-06 13:15:40 +0900

    [add]uiの作成

--- lib/ui/confession_screen.dart
+++ lib/ui/confession_screen.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:everyday_practice/domain/happy_unhappy_viewmodel.dart';
-
+import 'package:everyday_practice/domain/confession_viewmodel.dart';
 
 class ConfessionScreen extends HookConsumerWidget {
   const ConfessionScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final confession_value = ref.watch(ConfessionProvider);
     final title = ref.watch(HappyProvider.notifier).title;
+    final confession = ref.watch(ConfessionProvider.notifier).conffesion;
     return Scaffold(
       appBar: AppBar(
         title:Text(title),
@@ -17,25 +19,42 @@ class ConfessionScreen extends HookConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text('要因は？'),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Radio(
                   value: 1,
-                  groupValue: luck_value,
+                  groupValue: confession_value,
                   onChanged: (value) {
-                    ref.read(HappyProvider.notifier).update_happy(value!);
+                    ref.read(ConfessionProvider.notifier).update_confession(value!);
                   },
                 ),
-                Text('幸せ'),
+                Text('金'),
                 Radio(
                   value: 2,
-                  groupValue: luck_value,
+                  groupValue: confession_value,
                   onChanged: (value) {
-                    ref.read(HappyProvider.notifier).update_happy(value!);
+                    ref.read(ConfessionProvider.notifier).update_confession(value!);
                   },
                 ),
-                Text('不幸'),
+                Text('色欲'),
+                Radio(
+                  value: 3,
+                  groupValue: confession_value,
+                  onChanged: (value) {
+                    ref.read(ConfessionProvider.notifier).update_confession(value!);
+                  },
+                ),
+                Text('人間関係'),
+                Radio(
+                  value: 4,
+                  groupValue: confession_value,
+                  onChanged: (value) {
+                    ref.read(ConfessionProvider.notifier).update_confession(value!);
+                  },
+                ),
+                Text('災難'),
               ],
             ),
             SizedBox(
@@ -45,11 +64,24 @@ class ConfessionScreen extends HookConsumerWidget {
                   foregroundColor: Colors.white,
                   backgroundColor: Colors.deepOrangeAccent,
                 ),
-                child: const Text('next'),
-                onPressed: luck_value != 0 ? () {Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ConfessionScreen()),
-                );} : null,
+                child: const Text('回答'),
+                onPressed: confession_value != 0 ? () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text("あなたの回答"),
+                        content: Text("私は$title。要因は$confession。"),
+                        actions: [
+                          TextButton(
+                            child: Text("閉じる"),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                } : null,
               ),
             ),
           ],
@@ -58,3 +90,4 @@ class ConfessionScreen extends HookConsumerWidget {
     );
   }
 }
+
