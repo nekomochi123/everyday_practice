52613b3651b2ef4d9c3ececfd75c0d5cbbe575fa
nekomochi123 <akasama5968+work@gmail.com>
2024-04-06 11:42:22 +0900

    [add]viewmodel作成

--- lib/domain/happy_unhappy_viewmodel.dart
+++ lib/domain/happy_unhappy_viewmodel.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final happyProvider = StateProvider<int>((ref) => 0);
-final unhappyProvider = StateProvider<int>((ref) => 0);
+class RadioNotifier extends StateNotifier<int> {
+  RadioNotifier() : super(0);
 
+  void update_happy(int value) {
+    state = value;
+  }
+}
+
+final HappyProvider = StateNotifierProvider<RadioNotifier, int>((ref) => RadioNotifier());
