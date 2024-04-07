7ef348342b3fc4768871885d45ab4e4f996c1798
nekomochi123 <akasama5968+work@gmail.com>
2024-04-07 23:47:37 +0900

    [add]UI作成

--- lib/ui/hapiness/happiness_screen.dart
+++ lib/ui/hapiness/happiness_screen.dart
@@ -2,25 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 
-class MyHomePage extends HookConsumerWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class HappinessScreen extends HookConsumerWidget {
+  const HappinessScreen({super.key});
+
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Riverpod counter example'),
+        title: const Text('Are you happy?'),
       ),
-      body: Center(
-        child: Text(
-          '$count',
-          style: Theme.of(context).textTheme.headline4,
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text('あなたは幸せですか？不幸ですか？'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          Radio(
+            value: 1,
+            groupValue: null,
+            onChanged: (value) {
+            },
+          ),
+          Text('幸せ'),
+          Radio(
+            value: 2,
+            groupValue: null,
+            onChanged: (value) {
+            },
+          ),
+          Text('不幸'),
+      ],
+          ),
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+          onPressed: () {
+          },
+          child: const Text('選択'),
+        )
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.read(counterProvider.notifier).increment(),
-        child: const Icon(Icons.add),
-      ),
+        ],
+      )
     );
   }
 }
